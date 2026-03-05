94f247be64965d5f00f79d7279919f132a0f9ab7
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-03-04 20:06:00 -0800

    feat /disasters to easily join!

--- src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/Arcadedisasterplus.java
+++ src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/Arcadedisasterplus.java
@@ -1,10 +1,25 @@
 package com.simon.arcadedisasterplus.arcadedisasterplus;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class Arcadedisasterplus implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(literal("disasters").executes(context -> {
+                // Is this the right way to do this? Who knows!
+                // Also this might should have settings, not run on non-hypixel, and maybe detect when it won't work in some contexts
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.player != null) {
+                    client.player.networkHandler.sendChatCommand("/play arcade_disasters");
+                }
+                return 1;
+            }));
+        });
     }
 }
