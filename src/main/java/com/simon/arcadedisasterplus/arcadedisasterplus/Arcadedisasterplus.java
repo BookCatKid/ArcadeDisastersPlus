30adfa81f711aedae336b589c89917600e2ebe28
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-03-04 17:12:00 -0800

    init

---
+++ src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/Arcadedisasterplus.java
@@ -0,0 +1,10 @@
+package com.simon.arcadedisasterplus.arcadedisasterplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Arcadedisasterplus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/client/ArcadedisasterplusClient.java
@@ -0,0 +1,10 @@
+package com.simon.arcadedisasterplus.arcadedisasterplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcadedisasterplusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
