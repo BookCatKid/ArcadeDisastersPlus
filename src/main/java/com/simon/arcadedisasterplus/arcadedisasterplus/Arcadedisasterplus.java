92a54abdfa679bb14fb1e87f52801daebbf5b330
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-03-05 16:17:10 -0800

    fix command run (remove /)

--- src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/Arcadedisasterplus.java
+++ src/main/java/com/simon/arcadedisasterplus/arcadedisasterplus/Arcadedisasterplus.java
@@ -16,7 +16,7 @@ public class Arcadedisasterplus implements ModInitializer {
                 // Also this might should have settings, not run on non-hypixel, and maybe detect when it won't work in some contexts
                 MinecraftClient client = MinecraftClient.getInstance();
                 if (client.player != null) {
-                    client.player.networkHandler.sendChatCommand("/play arcade_disasters");
+                    client.player.networkHandler.sendChatCommand("play arcade_disasters");
                 }
                 return 1;
             }));
